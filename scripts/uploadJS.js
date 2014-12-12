fea61fa0947b437d564fc41e295129a4891431b2
Velizar Bishurov <vbishurov@gmail.com>
2014-12-12 14:40:55 +0200

    Major code refactor for upload form

--- scripts/uploadJS.js
+++ scripts/uploadJS.js
@@ -1,38 +1,7 @@
-// Declaring and defining global increment variable.
-var numOfPictures = 0;
-
-//  Add upload fields
+//Add upload fields
 $(document).ready(function () {
     $('#addMore').click(function () {
-        $(this).before($("<div/>", {id: 'filediv'}).
-            fadeIn('slow').
-            append($("<input/>", {name: 'file[]', type: 'file', id: 'file'}), $("<br/><br/>")));
-    });
-
-// Following function will executes on change event of file input to select different file.
-    $('body').on('change', '#file', function () {
-        if (this.files && this.files[0]) {
-            numOfPictures += 1; // Incrementing global variable by 1.
-            $(this).before("<div id='temp" + numOfPictures + "' class='temp'></div>");
-            var reader = new FileReader();
-            reader.onload = imageIsLoaded;
-            reader.readAsDataURL(this.files[0]);
-//                $(this).hide();
-            $("#temp" + numOfPictures).append($("<img/>", {
-                id: 'img',
-                src: 'x.png',
-                alt: 'delete'
-            }).click(function () {
-                $(this).parent().parent().remove();
-            }));
-        }
-    });
-
-    $('#upload').click(function (e) {
-        var name = $(":file").val();
-        if (!name) {
-            alert("First Image Must Be Selected");
-            e.preventDefault();
-        }
+        $(this).before($('<div/>', {id: 'filediv'}).fadeIn('slow').
+            append($('<input/>', {name: 'fileToUpload[]', type: 'file', id: 'fileToUpload'}), $('<br/><br/>')));
     });
 });
--- upload.php
+++ upload.php
@@ -1,59 +1,61 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-
-<!DOCTYPE html>
-<html>
 <head>
-    <title>Upload Multiple Images Form</title>
-    <!-------Including jQuery from Google ------>
+    <title>Multiple Image Upload Form</title>
+    <!--    Including jQuery from Google-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <link rel="stylesheet" href="styles/upload.css"/>
+</head>
 <body>
-<div id="maindiv">
-    <div id="formdiv">
-        <h2>Multiple Image Upload Form</h2>
+<div id="wrapper">
+    <h2>Multiple Image Upload Form</h2>
 
-        <form enctype="multipart/form-data" action="" method="post">
-            First Field is Compulsory. Only JPEG,PNG,JPG,GIF Type Image Allowed. Image Size Should Be Less Than 2MB.
-            <div id="filediv"><input name="file[]" type="file" id="file"/><br/><br/></div>
-            <input type="button" id="addMore" class="upload" value="Add More Files"/>
-            <input type="submit" value="Upload File" name="submit" id="upload" class="upload"/>
-        </form>
-    </div>
+    <form action="" method="post" enctype="multipart/form-data">
+        First Field is required. Allowed formats: JPEG, PNG, JPG, GIF. Image Size: Less that 2MB.
+        <div id="filediv"><input type="file" name="fileToUpload[]" id="fileToUpload"/><br/><br/></div>
+        <input type="button" id="addMore" class="upload" value="Add More Files"/>
+        <input type="submit" value="Upload File" name="submit" id="upload" class="upload"/>
+    </form>
 </div>
 </body>
 </html>
 
 <?php
 if (isset($_POST['submit'])) {
-    $index = 0;
-    $targetPath = "uploads/";     // Declaring Path for uploaded images.
+    $index = 0; //
+    $targetDir = 'uploads/'; // Folder for uploaded files
 
-// Loop to get individual element from the array
-    for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
-        $validExtensions = array("jpeg", "jpg", "png", "gif");      // Valid extensions
-        $targetFile = $targetPath . basename($_FILES['file']['name'][$i]);
-        $fileExtension = pathinfo($targetFile, PATHINFO_EXTENSION);
-        $check = getimagesize($_FILES["file"]["tmp_name"][$i]);
-        // Set the target path with a new name of image.
-        $targetPath = $targetPath . md5(uniqid()) . "." . $fileExtension;
+    for ($i = 0; $i < count($_FILES['fileToUpload']['name']); $i++) {
+        if ($_FILES['fileToUpload']['name'][$i]) {
+            $validExtension = array('jpeg', 'jpg', 'png', 'gif');
+            $targetFile = $targetDir . basename($_FILES['fileToUpload']['name'][$i]);
+            $fileExtension = pathinfo($targetFile, PATHINFO_EXTENSION);
+            $fileSize = $_FILES['fileToUpload']['size'][$i];
+            $checkIfImage = getimagesize($_FILES['fileToUpload']['tmp_name'][$i]);
+            $uploadOK = true;
+            $targetFile = $targetDir . md5(uniqid()) . '.' . $fileExtension;
 
-        $index++;      // Increment the number of uploaded images according to the files in array.
+            $index++; //Increment number of uploaded images
 
-        if ($check && ($_FILES["file"]["size"][$i] < 2000000) && in_array($fileExtension, $validExtensions)) {
-            if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $targetPath)) { // If file moved to uploads folder.
-                echo $index . ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
-            } else {     //  If File Was Not Moved.
-                echo $index . ').<span id="error">please try again!.</span><br/><br/>';
+            if (!$checkIfImage) {
+                $uploadOK = false;
+                echo $index . ').<span id="error">***Invalid file Type***</span><br /><br />';
+            } else if ($fileSize > 2000000) {
+                $uploadOK = false;
+                echo $index . ').<span id="error">***File too big***</span><br /><br />';
+            } else if (!in_array($fileExtension, $validExtension)) {
+                $uploadOK = false;
+                echo $index . ').<span id="error">***Invalid file Type***</span><br /><br />';
+            } else if ($uploadOK) {
+                if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'][$i], $targetFile)) { //If file moved to uploads folder successfully
+                    echo $index . ').<span id="noError">Image uploaded successfully!</span><br /><br />';
+                } else {
+                    echo $index . ').<span id="error">Error uploading file, please try again!</span><br /><br />';
+                }
             }
-        } else {     //   If File Size or File Type Was Incorrect.
-            echo $index . ').<span id="error">***Invalid file Size or Type***</span><br/><br/>';
         }
     }
 }
 ?>
-
 
 <script src="scripts/uploadJS.js"></script>

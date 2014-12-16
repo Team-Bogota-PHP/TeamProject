9214c9c51c0ec867eb64025b637c6d9fef61b220
veneling <v.grozev@outlook.com>
2014-12-16 22:55:39 +0200

    closed the input tags

--- widgets/login.php
+++ widgets/login.php
@@ -1,27 +1,20 @@
 <div class="widget">
-    <h2>Login/Register</h2>
-    <div class="inner">
-        <form action="login.php" method="post">
-            <ul id="login">
-                <li>
-                    Username:
-                    <input type="text" name="username"
-                </li>
-                <li>
-                    Password:
-                    <input type="password" name="password"
-                </li>
-                <li>
-                    <input type="submit" value="Log In"
-                </li>
-                <li>
-                    <a href='../../TeamProject/register.php'>Register</a>
-                </li>
-                <li>
-                    Forgotten your <a href="../../TeamProject/recover.php?mode=username">username</a> or <a href="../../TeamProject/recover.php?mode=password">password?</a>
+	<h2>Login/Register</h2>
+	<div class="inner">
+		<form action="login.php" method="post">
+			<ul id="login">
+				<li>Username: <input type="text" name="username" />
+				</li>
+				<li>Password: <input type="password" name="password" />
+				</li>
+				<li><input type="submit" value="Log In" /></li>
+				<li><a href='../../TeamProject/register.php'>Register</a></li>
+				<li>Forgotten your <a
+					href="../../TeamProject/recover.php?mode=username">username</a> or
+					<a href="../../TeamProject/recover.php?mode=password">password?</a>
 
-                </li>
-            </ul>
-        </form>
-    </div>
+				</li>
+			</ul>
+		</form>
+	</div>
 </div>
